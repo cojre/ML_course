c8e6ad49c41b8682c888f29e3ba8e6f4d4b9eafd
cojre <corentin.jaire@epfl.ch>
2022-10-13 23:25:36 +0200

    create homework 4

---
+++ labs/ex04/homework/build_polynomial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree.
+    
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        degree: integer.
+        
+    Returns:
+        poly: numpy array of shape (N,d+1)
+        
+    >>> build_poly(np.array([0.0, 1.5]), 2)
+    array([[1.  , 0.  , 0.  ],
+           [1.  , 1.5 , 2.25]])
+    """
+    phi = np.ones((len(x), degree+1))
+    for k in range(degree+1):
+        phi[:,k] = np.power(x, k)
+    return phi
